e4424f567c01b52e94817c21908bbfab826899cc
Dragon12346 <league@WTS-IM-6.attlocal.net>
2018-08-03 18:27:00 -0700

    Netflix/Movie(WIP)

---
+++ src/netflixrunner.java
@@ -0,0 +1,28 @@
+
+public class netflixrunner {
+public static void main(String[] args) {
+	Movie Moovie1 = new Movie("NotTwilight", 2);
+	Movie Moovie2 = new Movie("RPO", 3);
+	Movie Moovie3 = new Movie("TIG", 83);
+	Movie Moovie4 = new Movie("Shrek", 5);
+	Movie Moovie5 = new Movie("NotTwilight2", 2);
+	Moovie1.getTitle();
+	Moovie2.getTitle();
+	Moovie3.getTitle();
+	Moovie4.getTitle();
+	Moovie5.getTitle();
+	
+	Moovie1.getRating();
+	Moovie2.getRating();
+	Moovie3.getRating();
+	Moovie4.getRating();
+	Moovie5.getRating();
+	
+	Moovie1.getTicketPrice();
+	Moovie2.getTicketPrice();
+	Moovie3.getTicketPrice();
+	Moovie4.getTicketPrice();
+	Moovie5.getTicketPrice();
+	
+}
+}
